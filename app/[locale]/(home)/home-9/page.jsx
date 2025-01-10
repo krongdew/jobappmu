e2094d1591ed324480b833643560fc90e1ca97d3
krongdew <dewwiisunny14@gmai.com>
2025-01-10 15:10:44 +0700

    link

--- app/[locale]/(home)/home-9/page.jsx
+++ app/[locale]/(home)/home-9/page.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import Home from "@/components/home-9";
 
 export const metadata = {
-  title: "Home-9 || Superio - Job Borad React NextJS Template",
-  description: "Superio - Job Borad React NextJS Template",
+  title: "MUJobSphere || ค้นหางาน",
+  description: "MUJobSphere  - ค้นหางานสำหรับนักศึกษามหิดล",
 };
 
 const index = () => {
--- components/button/Homebutton.jsx
+++ components/button/Homebutton.jsx
@@ -1,5 +1,6 @@
 // UserTypeButtons.jsx
 import React from 'react';
+import Link from 'next/link';
 import "./style.css"
 import { useTranslations } from 'next-intl';
 
@@ -17,11 +18,13 @@ const UserTypeButtons = () => {
           </div>
         </div>
         <div className="col-12 col-md-6">
+        <Link href="/th/job-list-v2" style={{ textDecoration: 'none' }}>
           <div className="user-type-btn d-flex flex-column align-items-center justify-content-center">
             <div className="subtitle mb-2">{t('For')}</div>
             <div className="title">{t('Find a job')}</div>
             <div className="subtitle ">{t('Mahidol student only')}</div>
           </div>
+          </Link>
         </div>
       </div>
     </div>
